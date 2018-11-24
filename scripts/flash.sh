96b5263d768b1824e60da42a37bc0a0a2276d974
Lance R. Vick <lance@lrvick.net>
2018-11-23 16:56:25 -0800

    fix flash.sh for new paths

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -3,8 +3,8 @@ set -e
 cd "$HOME"
 
 device="${DEVICE?}"
-build_type="${BUILD_TYPE?}"
-build_variant="${BUILD_VARIANT?}"
+build_type="${BUILD_TYPE:-release}"
+build_variant="${BUILD_VARIANT:-user}"
 
 # shellcheck disable=SC1091
 source build/envsetup.sh
